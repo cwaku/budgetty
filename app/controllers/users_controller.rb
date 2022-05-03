8591649ed55cff7e36fa85c09b568dc75a79b275
Ricky Mormor <rickymormor1@gmail.com>
2022-05-03 16:38:21 +0100

    remove confirmable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,19 +20,19 @@ class UsersController < ApplicationController
   end
 
   # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
