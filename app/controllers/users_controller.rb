6b3825931060680c8e88039461d2a6754fb465fd
Ricky Mormor <rickymormor1@gmail.com>
2022-05-05 15:50:03 +0100

    Add expeses controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,21 +17,6 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit; end
 
-  # POST /users or /users.json
-  # def create
-  #   @user = User.new(user_params)
-
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
@@ -56,11 +41,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
 
   # Only allow a list of trusted parameters through.
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  unauthenticated do
+    root to: 'user#index'
+  end
+  
+  resources :users
+
   resources :categories_expenses
   resources :expenses
-  resources :categories
-  devise_for :users
-  resources :users
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :expenses, only: [:new, :create, :destroy, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index", as: "category_track"
 end
