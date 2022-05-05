2d5d7102a38e14b7df145f8e7d4dbb2e1ae0ebf5
Ricky Mormor <rickymormor1@gmail.com>
2022-05-05 14:29:54 +0100

    Add expenses and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :expenses
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
