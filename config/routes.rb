fea53a072a87c0cc8f57b3989a7d9bc54321a8e1
Ricky Mormor <rickymormor1@gmail.com>
2022-05-05 17:16:48 +0100

    Add view for category index

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users', to: 'users#index'
+    get '/', to: 'users#index'
 
-  unauthenticated do
-    root to: 'user#index'
+    authenticated :user do
+      get '/users/sign_out', to: 'devise/sessions#destroy'
+      root 'categories#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'users#index', as: :unauthenticated_root
+    end
   end
-  
-  resources :users
 
-  resources :categories_expenses
+
+  # resources :categories_expenses
   resources :expenses
-  resources :categories, only: [:index, :show, :new, :create, :destroy] do
-    resources :expenses, only: [:new, :create, :destroy, :show]
-  end
+  resources :categories
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index", as: "category_track"
+  # root "categories#index", as: "category_track"
 end
