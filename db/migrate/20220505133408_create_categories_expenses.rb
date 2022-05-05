f6a8108e56187800cd1ba94f97d5ae748f278cb6
Ricky Mormor <rickymormor1@gmail.com>
2022-05-05 14:50:06 +0100

    Finish relations

--- db/migrate/20220505133408_create_categories_expenses.rb
+++ db/migrate/20220505133408_create_categories_expenses.rb
@@ -1,6 +1,8 @@
 class CreateCategoriesExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :categories_expenses do |t|
+      t.references :category, foreign_key: true, null: false
+      t.references :expense, foreign_key: true, null: false
 
       t.timestamps
     end
